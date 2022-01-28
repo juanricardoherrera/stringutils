9fe05fd8e5a86ba0aa821cf16fd53a968cc62f36
Juan Ricardo Herrera <juanricardoherrera@gmail.com>
2022-01-28 08:39:01 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package stringutils
+
+import "strings"
+
+func Upper(world string) string {
+	return strings.ToUpper(world)
+}
+
+func Lower(world string) string {
+	return strings.ToLower(world)
+}
